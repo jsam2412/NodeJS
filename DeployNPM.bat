78e2bafc5aa16e76363759d0f33eb97a07133c61
J.Sam <j.sam2412@gmail.com>
2019-06-21 21:13:57 +0530

    Update DeployNPM.bat

--- DeployNPM.bat
+++ DeployNPM.bat
@@ -1,3 +1,3 @@
-pm2 start index.js
-pm2 start npm --name "SampleNodeJS" -- start
-pm2 save
+"C:\Users\sanyam.jain\AppData\Roaming\npm\node_modules\pm2\bin\pm2" start "C:\Sam\Work\Demo Project Source Code\MyNodeJS\NodeJS\index.js"
+"C:\Users\sanyam.jain\AppData\Roaming\npm\node_modules\pm2\bin\pm2" start npm --name "SampleNodeJS" -- start
+"C:\Users\sanyam.jain\AppData\Roaming\npm\node_modules\pm2\bin\pm2" save
